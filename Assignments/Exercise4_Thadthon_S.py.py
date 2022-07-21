344c883fae857802568940cacc6d67ee740b6385
thadthon <91382929+thadthon@users.noreply.github.com>
2022-07-21 11:35:44 +0700

    Create Exercise4_Thadthon_S.py.py

---
+++ Assignments/Exercise4_Thadthon_S.py.py
@@ -0,0 +1,10 @@
+FE = 89.5        #Foundation English
+GB = 90          #General Business
+ICS = 85.5       #Introduction to Computer Systems
+CP = 89.5        #Computer Programming
+
+print("---Your Score---")
+print("Foundation English                 :", FE)
+print("General Business                   :", GB)
+print("Introduction to Computer Systems   :", ICS)
+print("Computer Programming               :", CP)
